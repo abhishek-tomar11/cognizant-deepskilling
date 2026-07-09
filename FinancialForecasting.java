f1656abb0fd8158ca164ebb06b390a5f5933eba7
Your Name <abhishek2006tomar@gmail.com>
2026-07-09 05:49:05 +0530

    Adding week-3 file

---
+++ FinancialForecasting.java
@@ -0,0 +1,15 @@
+//package Week-third;
+
+public class FinancialForecasting {
+    public static void main(String[] args) {
+        double currentValue = 1000;   // for Initial investment
+        double growthRate = 0.10;     // as 10% growth per year
+        int years = 5;                // Forecast for 5 years
+
+        // Call recursive method
+        double futureValue = Forecast.predictFutureValue(currentValue, growthRate, years);
+
+        // Print result
+        System.out.println("Future Value after " + years + " years: " + futureValue);
+    }
+}
